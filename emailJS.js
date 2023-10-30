7c11973f24fea6f3c826e0230307fbba0e4c579b
Alex Gavin <a_gavin@icloud.com>
2023-10-29 20:39:03 -0700

    Try to stop some web scrapers

--- emailJS.js
+++ emailJS.js
@@ -1,7 +1,9 @@
 function copyText() {
 
     /* email value */
-    var copyText = "a_gavin@icloud.com";
+    var uname    = "a_gavin";
+    var domain   = "icloud.com";
+    var copyText = uname + "@" + domain;
   
      /* copy the text inside the text field */
     navigator.clipboard.writeText(copyText);
